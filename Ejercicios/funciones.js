bfac82dff61beb3461a5a65aa09f2e85ac8d31e4
emilianomillan <emilianomillan5@gmail.com>
2025-09-19 09:59:18 -0600

    Ejercicio - "Funcion de multiplicacion y de cuadrado"

---
+++ Ejercicios/funciones.js
@@ -0,0 +1,15 @@
+function multiplica(num1, num2){
+    return num1 * num2;
+}
+
+const multiplicaArrow = (num1, num2) => {
+    return num1 * num2;
+}
+
+function elevaCuadrado(num){
+    return num * num;
+}
+
+const elevaCuadradoArrow = (num) => {
+    return num * num;
+}
