cbf19d1c2b4cf6391c8926c491152e2305f958ab
emilianomillan <emilianomillan5@gmail.com>
2025-11-15 21:20:26 -0600

    Add files via upload

---
+++ PruebasUnitarias/funciones-varias.js
@@ -0,0 +1,39 @@
+function esNumeroPar(numero) {
+  return numero % 2 === 0;
+}
+
+function crearPerfil(nombre, edad) {
+  return {
+    nombre: nombre,
+    edad: edad
+  };
+}
+
+function buscarFruta(fruta) {
+  const refrigerador = ["manzana", "banana", "pera"];
+  
+  if (refrigerador.includes(fruta)) {
+    return fruta;
+  } else {
+    return null;
+  }
+}
+
+function obtenerPrimerElemento(lista) {
+  return lista[0];
+}
+
+function validarEdad(edad) {
+  if (edad < 0) {
+    throw new Error("La edad no puede ser negativa");
+  }
+  return "Edad válida";
+}
+
+module.exports = {
+  esNumeroPar,
+  crearPerfil,
+  buscarFruta,
+  obtenerPrimerElemento,
+  validarEdad
+};
